10ba9f25a7954cf994c02b0cc93d40da38438a35
Dulappagari Raju <rajud45117@gmail.com>
2024-07-08 13:42:14 +0530

    added the adding and substracting files

---
+++ addingFile.py
@@ -0,0 +1,10 @@
+a=200
+b=300
+if a>b:
+    print("a is bigger")
+else:
+    print("b is bigger")
+
+sum =a+b
+print(f'adding of two numbers is {sum}')
+
---
+++ substractFile.py
@@ -0,0 +1,10 @@
+a=400
+b=300
+if a>b:
+    print("a is bigger")
+else:
+    print("b is bigger")
+
+sub=a-b
+print(f"substracting of two numbers is {sub}")
+     
