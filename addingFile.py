2a7b72774358ab005102efb1c571b263e6caf66d
Dulappagari Raju <rajud45117@gmail.com>
2024-07-08 13:45:46 +0530

    added the adding and substracting files

--- addingFile.py
+++ addingFile.py
@@ -4,7 +4,9 @@ if a>b:
     print("a is bigger")
 else:
     print("b is bigger")
+    added the adding and substracting files
 
+print("Adding a two files")
 sum =a+b
 print(f'adding of two numbers is {sum}')
 
