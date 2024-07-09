6525e824a8f1b8719e108ded62d88b5e356ca710
Dulappagari Raju <rajud45117@gmail.com>
2024-07-09 15:06:46 +0530

    Revert "added the adding and substracting files"
    This reverts commit 2a7b72774358ab005102efb1c571b263e6caf66d.

--- addingFile.py
+++ addingFile.py
@@ -4,9 +4,7 @@ if a>b:
     print("a is bigger")
 else:
     print("b is bigger")
-    added the adding and substracting files
 
-print("Adding a two files")
 sum =a+b
 print(f'adding of two numbers is {sum}')
 
